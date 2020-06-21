d1fb28a7cef4baa6cd7871e009cf3e69a487d99d
Jeffrey Fabian <jf2978@columbia.edu>
2020-06-20 21:05:52 -0400

    add resource card data, update grid w/ queried content

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -13,6 +13,14 @@ module.exports = {
     // If you want to use styled components you should add the plugin here.
     // 'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `json`,
+        path: `${__dirname}/src/json`,
+      },
+    },
   ],
   siteMetadata: {
     title: 'APM Map',
--- website/src/components/DirectoryGrid.js
+++ website/src/components/DirectoryGrid.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useStaticQuery, graphql } from "gatsby"
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -8,6 +9,8 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import { CardActionArea } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   cardGrid: {
@@ -29,28 +32,45 @@ const useStyles = makeStyles((theme) => ({
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-export default function DirectoryGrid() {
-  const classes = useStyles();
+  export default function DirectoryGrid() {
+    const classes = useStyles();
+    const data = useStaticQuery(graphql`
+      query ResourcesQuery {
+        allResourcesJson {
+          edges {
+            node {
+              Name
+              Link
+              Type
+              Tags
+              Description
+            }
+          }
+        }
+      }
+    `)
 
   return (
-    <Container className={classes.cardGrid} maxWidth="md">
+    <Container className={classes.cardGrid} maxWidth="lg">
       <Grid container spacing={4}>
-        {cards.map((card) => (
-          <Grid item key={card} xs={12} sm={6} md={4}>
+        {data.allResourcesJson.edges.map((card, index) => (
+          <Grid item key={index} xs={12} sm={6} md={4}>
             <Card className={classes.card}>
-              <CardMedia
-                className={classes.cardMedia}
-                image="https://source.unsplash.com/random"
-                title="Image title"
-              />
-              <CardContent className={classes.cardContent}>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Heading
-                </Typography>
-                <Typography>
-                  This is a media card. You can use this section to describe the content.
-                </Typography>
-              </CardContent>
+              <CardActionArea href={card.node.Link}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image="https://source.unsplash.com/random"
+                  title={card.node.Name}
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {card.node.Name}
+                  </Typography>
+                  <Typography>
+                    {card.node.Description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
               <CardActions>
                 <Button size="small" color="primary">
                   View
