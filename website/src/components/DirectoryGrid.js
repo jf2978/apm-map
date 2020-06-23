553e6b3361f40475317ff5a5e4f9f46fe0008266
Jeffrey Fabian <jf2978@columbia.edu>
2020-06-23 17:34:01 -0400

    add paperbase theme and apply globally

--- website/src/components/DirectoryGrid.js
+++ website/src/components/DirectoryGrid.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useStaticQuery, graphql } from "gatsby"
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
