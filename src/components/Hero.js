304d299f1c27f2ad6c7539add868f98ee2618f93
Jeffrey Fabian <jf2978@columbia.edu>
2020-07-14 20:18:44 -0400

    add transition to hero text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Fade from '@material-ui/core/Fade';
+import { getThemeProps } from '@material-ui/styles';
+import { Grow } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   hero: {
@@ -18,12 +21,19 @@ export default function Hero({ id, title, subtitle, emoji }) {
 
   return (
     <Box id={id} className={classes.hero}>
-      <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-        {title} {emoji}
-      </Typography>
-      <Typography variant="h6" align="center" color="textSecondary" paragraph>
-        {subtitle}
-      </Typography>
+      <Grow
+        in={true}
+        timeout={1600}
+      >
+        <div>
+          <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+            {title} {emoji}
+          </Typography>
+          <Typography variant="h6" align="center" color="textSecondary" paragraph>
+            {subtitle}
+          </Typography>
+        </div>
+      </Grow>
     </Box>
   );
 }
