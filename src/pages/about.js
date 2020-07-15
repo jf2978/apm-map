d6aef945ab541ea7c8d9b109c0365f4572a8bc8e
Jeffrey Fabian <jf2978@columbia.edu>
2020-07-15 18:58:15 -0400

    make about page ui responsive + remove placeholder divs

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import { ThemeProvider } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 
 import theme from '../theme'
@@ -13,32 +14,38 @@ import Emoji from '../components/Emoji';
 import Hero from '../components/Hero';
 import Blurb from '../components/Blurb';
 
+const useStyles = makeStyles((theme) => ({
+  bio: {
+    [theme.breakpoints.up("xs")]: {
+      padding: theme.spacing(4,2,4),
+      margin: theme.spacing(2,2,2)
+    },
+    [theme.breakpoints.up("sm")]: {
+      padding: theme.spacing(4,2,4),
+      margin: theme.spacing(8,8,8)
+    },
+  },
+}));
+
 export default function About() {
+  const classes = useStyles();
+
   return (
     <Layout>
       <Context.Consumer>
         {context => (
           <>
-            <Hero
-              id="about-hero"
-              title="insert slogan here..."
-              subtitle="Interested in supporting us? Become a mentor or make a donation!"
-            />
             <Blurb
               title="About APM Map"
               description="APM Map is your one-stop destination for navigating your A/PM recruiting journey. Whether you're looking to better understand the role of a PM or to refine your System Design interviewing skills for a technical round, we've curated resources across the entire A/PM recruiting timeline to help you accomplish just that. "
               emoji={<Emoji symbol="🗺️" label="map"/>}
             />
-            <Box height={500} bgcolor="lightblue">
-              <Container>
-                Michelle Ma
-              </Container>
-            </Box>
-            <Box height={500} bgcolor="lightgreen">
-              <Container>
-                Jeff
-              </Container>
-            </Box>
+            <Box
+              height={500}
+              bgcolor="lightblue"
+              display="flex"
+              justifyContent="space-between"
+            />
           </>
         )}
       </Context.Consumer>
