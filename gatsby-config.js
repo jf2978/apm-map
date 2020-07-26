5cf0c37b05abf566a22a1edfab745a3b3d65e550
Jeffrey Fabian <jf2978@columbia.edu>
2020-07-25 23:34:53 -0400

    add images to local /static folder + test page query with gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/static/images`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `APM Map`,
@@ -34,9 +41,11 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        icon: `static/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
   ],
   siteMetadata: {
     title: "APM Map",
