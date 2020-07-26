20999763515fae57c106ed830acfc6067497d2cc
Jeffrey Fabian <jf2978@columbia.edu>
2020-07-25 22:30:35 -0400

    npm install framer motion + add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,20 @@ module.exports = {
         path: `${__dirname}/src/json`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `APM Map`,
+        short_name: `APM Map`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
   ],
   siteMetadata: {
     title: "APM Map",
