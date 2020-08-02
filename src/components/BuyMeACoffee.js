2f9b4d48f69871de90d9d1fe8dbc7c59b1064c4e
jeffrey-fabian <jfabian@rsglab.com>
2020-08-02 19:51:55 -0400

    add html tooltip to buymeacoffee btn

--- src/components/BuyMeACoffee.js
+++ src/components/BuyMeACoffee.js
@@ -1,27 +1,38 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import IconButton from "@material-ui/core/IconButton";
-import Button from "@material-ui/core/Button";
-import { makeStyles, darken } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Tooltip from "@material-ui/core/Tooltip";
+import { withStyles, makeStyles, darken } from "@material-ui/core/styles";
 
 import BuyMeACoffeeIcon from "../../assets/bmac.svg";
-import { motion } from "framer-motion";
+import Emoji from "../components/Emoji";
 
 const useStyles = makeStyles((theme) => ({
   button: {
-    borderRadius: theme.shape.borderRadius,
     boxShadow: theme.shadows[10],
-    width: 100,
-    height: 90,
-    backgroundColor:"#79D6B5",
+    width: 80,
+    height: 80,
+    backgroundColor: "#79D6B5",
     float: "right",
     margin: theme.spacing(3),
     "&:hover": {
-      backgroundColor: darken("#79D6B5", 0.1),
+      backgroundColor: darken("#79D6B5", 0.05),
       boxShadow: theme.shadows[10],
     },
   },
 }));
+
+const HTMLTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: "#f5f5f9",
+    maxWidth: 180,
+    color: "rgba(0, 0, 0, 0.87)",
+    fontSize: theme.typography.pxToRem(12),
+    border: "1px solid #dadde9",
+  },
+}))(Tooltip);
 
 export default function BuyMeACoffee() {
   const classes = useStyles();
@@ -48,26 +59,32 @@ export default function BuyMeACoffee() {
       y: 0,
       transition: {
         from: 0,
-        to: 5,
+        to: 2,
         yoyo: Infinity,
-        duration: 1,
+        duration: 0.5,
       },
     },
   };
 
   return (
-    <motion.div
-      animate={["after", "bounce"]}
-      variants={buttonVariants}
-    >
-      <Button
-      variant="contained"
-      color="red"
-      className={classes.button}
-      href="https://www.buymeacoffee.com/michellema"
-    >
-        <BuyMeACoffeeIcon height={80} width={80} />
-      </Button>
+    <motion.div animate={["after", "bounce"]} variants={buttonVariants}>
+      <HTMLTooltip
+        title={
+          <Typography variant="body2">
+            {" If we've helped you in your journey, consider suppprting ours! "}
+            <Emoji symbol="❤️" label="heart" />
+          </Typography>
+        }
+        placement="left"
+        aria-label="help-support-us"
+      >
+        <IconButton
+          className={classes.button}
+          href="https://www.buymeacoffee.com/michellema"
+        >
+          <BuyMeACoffeeIcon height={75} width={75} />
+        </IconButton>
+      </HTMLTooltip>
     </motion.div>
   );
 }
