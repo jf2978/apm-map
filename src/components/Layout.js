a4b139d695a178fd0dba9f95aa6a5390b046d5bc
jeffrey-fabian <jfabian@rsglab.com>
2020-08-02 20:25:56 -0400

    add BMAC + fake input form to layout (footer)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Link } from "gatsby";
+
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+
 import Copyright from "./Copyright";
 import { Context } from "../../provider";
-import { Link } from "gatsby";
+import BuyMeACoffee from "../components/BuyMeACoffee";
 
 // fun fact I didn't know of: "a11y" is SWE lingo for accessibility
 // In this case, ARIA = Accessible Rich Internet Application and the set of attributes
@@ -37,12 +45,24 @@ const useStyles = makeStyles((theme) => ({
   },
   footer: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
+    padding: theme.spacing(2),
+  },
+  newsletter: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  btn: {
+    margin: theme.spacing(1),
   },
 }));
 
-export default function Layout({ value, handleChange, children }) {
+export default function Layout({ children }) {
   const classes = useStyles();
+  const [email, setEmail] = React.useState("Email");
+
+  const handleChange = (event) => {
+    setEmail(event.target.value);
+  };
 
   return (
     <Context.Consumer>
@@ -68,8 +88,22 @@ export default function Layout({ value, handleChange, children }) {
             </Tabs>
           </AppBar>
           <main>{children}</main>
+          <BuyMeACoffee />
+
           <footer className={classes.footer}>
-            <Copyright />
+            <Box className={classes.newsletter}>
+              <form noValidate autoComplete="off">
+                <FormControl variant="outlined">
+                  <InputLabel htmlFor="component-outlined">Email</InputLabel>
+                  <OutlinedInput onChange={handleChange} label="Email" />
+                </FormControl>
+              </form>
+              <Box className={classes.btn}>
+                <Button variant="contained" size="medium" color="primary">
+                  Sign up
+                </Button>
+              </Box>
+            </Box>
           </footer>
         </Container>
       )}
