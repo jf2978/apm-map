9252766216a2b453b7393753eeb5c78c0b0256c9
jeffrey-fabian <jfabian@rsglab.com>
2020-08-03 18:43:30 -0400

    add drawed out svg path

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -4,6 +4,8 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import { Grow } from "@material-ui/core";
 
+import PathSVG from "../../assets/timeline-path.svg";
+
 const useStyles = makeStyles((theme) => ({
   box: {
     [theme.breakpoints.up("xs")]: {
@@ -13,10 +15,24 @@ const useStyles = makeStyles((theme) => ({
       padding: theme.spacing(4, 2, 4),
     },
   },
+  svg: {
+    strokeDasharray: 1000,
+    strokeDashoffset: 1000,
+    animation: `$dash 2s linear infinite`,
+  },
+  "@keyframes dash": {
+    to: {
+      strokeDashoffset: 0,
+    },
+  },
 }));
 
 export default function Timeline() {
   const classes = useStyles();
 
-  return <Box height={400} bgcolor="lightgreen" className={classes.box}></Box>;
+  return (
+    <Box height={400} bgcolor="lightgreen" className={classes.box}>
+      <PathSVG className={classes.svg}></PathSVG>
+    </Box>
+  );
 }
