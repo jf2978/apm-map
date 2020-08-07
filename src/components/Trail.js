571270316d29aee00f6b972d4b550f40ff712af2
Jeffrey Fabian <jf2978@columbia.edu>
2020-08-06 20:51:22 -0400

    not quite, but got a circle that animates across the SVG path

--- src/components/Trail.js
+++ src/components/Trail.js
@@ -36,11 +36,17 @@ export default function Trail() {
     <>
       <svg viewBox="0 0 725 190" className={classes.path}>
         <motion.path
+          id="trail"
           style={{ pathLength }}
           d="M70.5,260.5c24.94-36.09,64.72-82.66,109-79,65.51,5.42,85.51,116.21,144,116,59.65-.22,82.63-115.61,138-112,52.14,3.4,69.64,108.22,116,105,37.11-2.58,47.71-71.24,86-71,32.35.2,40,49.31,71,51,47.82,2.61,67-111.81,118-115,47.09-2.95,61.4,92.82,123,113,82.81,27.12,154.88-159.27,231-144,25.57,5.13,59.69,33.41,85,151"
           transform="translate(-70.09 -123.11)"
           fill="none"
         />
+        <motion.circle transform="translate(-70.09 -123.11)" r="5" fill="red">
+          <motion.animateMotion dur="5s" repeatCount="indefinite">
+            <motion.mpath xlinkHref="#trail" />
+          </motion.animateMotion>
+        </motion.circle>
       </svg>
       <svg viewBox="0 0 725 190" className={classes.pathOverlay}>
         <path
