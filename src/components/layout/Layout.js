86b5cbfb0d2e9a3e4b6ec69884142dbea8f101db
jeffrey-fabian <jfabian@rsglab.com>
2020-08-23 19:15:47 -0400

    arbitrary change to redploy

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 
 import { Context } from "./Provider";
